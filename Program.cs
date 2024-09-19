9dcb7571108e20213d4d36d8d1fe74e63549ab4e
Luiz Ricardo de Campos <playsonbr@hotmail.com>
2024-09-19 12:18:33 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+int numero = 10;
+bool ehPar = false;
+
+//IF TERNARIO
+ehPar = numero % 2 == 0;
+Console.WriteLine($" O numero {numero} é " + (ehPar ? "Par" : "ímpar"));
+
+
+
+//IF NORMAL
+// if(numero % 2 == 0)
+// {
+//     Console.WriteLine($"O numero {numero} é Par");
+// } else
+// {
+//     Console.WriteLine($"O numero {numero} é Impar");
+// }
